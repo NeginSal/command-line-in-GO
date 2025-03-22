62d0612fd98813c43437ee32e3979334dc6ad915
NeginSal <neginsaljooghi@gmail.com>
2025-03-22 16:50:17 +0330

    add calculate function

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello,ninjas !")
+	fmt.Println(calculatePrice(1, 2, 3))
+}
+
+func calculatePrice(x, y, z int) int {
+	return x + y + z
 }
