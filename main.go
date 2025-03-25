166d9d484f3825b96d06dd575ea5f5745aafc86b
NeginSal <neginsaljooghi@gmail.com>
2025-03-25 11:43:37 +0330

    conditions & loops

--- main.go
+++ main.go
@@ -5,27 +5,19 @@ import (
 )
 
 func main() {
-	// x := 0
-	// for x < 5 {
-	// 	fmt.Println("the value of x is :", x)
-	// 	x++
-	// }
+	age := 45
 
-	// for i := 0; i < 5; i++ {
-	// 	fmt.Println("value of x is:", i)
-	// }
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 45)
 
-	names := []string{"mario", "luigi", "peach", "yoshi"}
-	// for i := 0; i < len(names); i++ {
-	// 	fmt.Println(names[i])
-	// }
-
-	// for index, value := range names {
-	// 	fmt.Printf("the value at index %v is %v \n", index, value)
-	// }
-
-	for _,value := range names {
-		fmt.Printf("the value is  %v  \n",value)
+	if age < 30 {
+		fmt.Println("age is less than 30")
+	} else if age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("age is not less than 40")
 	}
 
 }
