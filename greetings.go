15ad8294975a2e5868ccfe1b4ec359d1ae5e077c
NeginSal <neginsaljooghi@gmail.com>
2025-03-29 10:24:57 +0330

    package scope

---
+++ greetings.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var points = []int{20, 90, 100, 50}
+
+func sayHello(n string) {
+	fmt.Println("hello", n)
+}
+
+func showScore() {
+	fmt.Println("score is : ", score)
+}
--- main.go
+++ main.go
@@ -1,27 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func getInitials(n string) (string, string) {
-	s := strings.ToUpper(n)
-	names := strings.Split(s, " ")
-
-	var initials []string
-	for _, v := range names {
-		initials = append(initials, v[:1])
-	}
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-
-	return initials[0], "_"
-}
+var score = 99.5
 
 func main() {
-	fn, sn := getInitials("tifa lockhart")
-	fmt.Println(fn, sn)
+	sayHello("mario")
 
+	for _, v := range points {
+		fmt.Println(v)
+	}
+
+	showScore()
 }
