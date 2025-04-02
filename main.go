eaa6085e775e32cac23d1362ae4d2d48fafad708
NeginSal <neginsaljooghi@gmail.com>
2025-04-02 10:32:13 +0330

    Receiver functions with pointers

--- main.go
+++ main.go
@@ -4,5 +4,12 @@ import "fmt"
 
 func main() {
 	mybill := newBill("mario's bill")
+
+	mybill.addItem("onion soup", 4.50)
+	mybill.addItem("veg pie", 5.35)
+	mybill.addItem("pudding", 3.25)
+	mybill.addItem("tea", 7.00)
+	mybill.updateTip(10)
+
 	fmt.Println(mybill.format())
 }
